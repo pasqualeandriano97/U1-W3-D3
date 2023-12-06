f07f2cc973d4e3c4772a4d8a85f2d7b291b7e872
pasqualeandriano97 <andrianopasquale97@gmail.com>
2023-12-07 00:29:24 +0100

    Lavoro completamente rivisto nella parte grafica

--- js1.js
+++ js1.js
@@ -6,21 +6,25 @@ taskInput.addEventListener("submit", function (e) {
   const currentTask = document.getElementById("taskName");
 
   console.log(currentTask.value);
-  const taskListItem = document.createElement("p");
+  const taskListItem = document.createElement("li");
   taskListItem.innerText = currentTask.value;
+  if (currentTask.value === " ") {
+    alert("Inserisci un task prima di salvarlo");
+  } else {
+    const taskListSection = document.getElementById("taskList");
 
-  const taskListSection = document.getElementById("taskList");
+    taskListSection.appendChild(taskListItem);
+    currentTask.value = " ";
 
-  taskListSection.appendChild(taskListItem);
-  currentTask.value = " ";
-
-  taskListItem.addEventListener("click", function (e) {
-    taskListItem.style.textDecoration = "line-through";
-  });
+    taskListItem.addEventListener("click", function (e) {
+      taskListItem.classList.toggle("barred");
+    });
+  }
 
   const button = document.createElement("button");
   button.classList.add("button");
-  button.innerText = "elimina";
+  button.innerText = "Elimina";
+
   taskListItem.appendChild(button);
 
   button.addEventListener("click", function (e) {
